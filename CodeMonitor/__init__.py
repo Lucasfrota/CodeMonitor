783a6dd08201262f7a4a41ebe6a1b1850fca36c7
Lucas Frota <lucv.frota@gmail.com>
2020-05-22 19:14:17 -0400

    Update __init__.py

--- CodeMonitor/__init__.py
+++ CodeMonitor/__init__.py
@@ -1,1 +1,10 @@
-name = "CodeMonitor"
+import setuptools
+
+setuptools.setup(
+  name="CodeMonitor",
+  version="1.0.0",
+  author="Lucas Frota",
+  author_email="lucv.frota@gmail.com",
+  description="Code Monitor is a simple way to send feedback about your code to your smartphone as a Telegram message!",
+  url="https://github.com/Lucasfrota/CodeMonitor"
+)
